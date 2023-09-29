7508e220a6f16752d983f313bb74b940508828f0
kei-600th <kei19941005@gmail.com>
2023-09-29 17:47:55 +0900

    eslintとprettierの設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,13 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    quotes: ["error", "double"],
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: false,
+      },
+    ],
   },
   overrides: [
     {
