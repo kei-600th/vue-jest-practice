843ba7905e1cda03a6958a046ff49bf43f7e1e08
kei-600th <kei19941005@gmail.com>
2023-09-29 10:44:18 +0900

    4_todolist_4

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -2,7 +2,7 @@ import { mount } from "@vue/test-utils";
 import TodoList from "@/views/TodoList.vue";
 
 describe("TodoList.vue", () => {
-  it("xxx", () => {
+  it("should show todo items", () => {
     const wrapper = mount(TodoList);
     expect(wrapper.text()).toMatch("todo item");
     expect(wrapper.text()).toMatch("todo item2");
