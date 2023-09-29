d351c5d2d7b8eccda4609993abb950746cfc16fd
kei-600th <kei19941005@gmail.com>
2023-09-29 11:52:12 +0900

    7_todolist_7

--- tests/unit/todo.spec.ts
+++ tests/unit/todo.spec.ts
@@ -1,11 +1,17 @@
-import { mount } from "@vue/test-utils";
+import { flushPromises, mount } from "@vue/test-utils";
 import TodoList from "@/views/TodoList.vue";
+import * as api from "@/apis/TodoApi";
 
 describe("TodoList.vue", () => {
   it("should show todo items", async () => {
+    //given
+    jest.spyOn(api, "getTodoList").mockResolvedValueOnce([
+      { id: 1, name: "todo item" },
+      { id: 2, name: "todo item2" },
+    ]);
     //when
     const wrapper = mount(TodoList);
-    await wrapper.vm.$nextTick();
+    await flushPromises();
     //then;
     expect(wrapper.text()).toMatch("todo item");
     expect(wrapper.text()).toMatch("todo item2");
